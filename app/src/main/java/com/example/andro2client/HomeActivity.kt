fc5bbdde596536c67a8c914fbb09a27d0686982f
adimor1 <adimor1>
2022-05-23 14:01:54 +0300

    add image

--- app/src/main/java/com/example/andro2client/HomeActivity.kt
+++ app/src/main/java/com/example/andro2client/HomeActivity.kt
@@ -6,9 +6,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
@@ -18,9 +16,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.ImagePainter
+import coil.compose.rememberImagePainter
 import com.example.andro2client.ui.theme.Andro2ClientTheme
 import com.example.andro2client.ui.theme.MainScreen
 import com.fasterxml.jackson.core.type.TypeReference
@@ -100,23 +102,25 @@ if(mylistdata!=null){
                     .fillMaxWidth()
                     .wrapContentHeight(),
                 shape = MaterialTheme.shapes.medium,
-                elevation = 5.dp,
+                elevation = 8.dp,
                 backgroundColor = MaterialTheme.colors.background
             )
             {
                 Row(
-
                     Modifier.clickable {
                         val intent = Intent(context, RecipeActivity::class.java)
                         intent.putExtra("RECIPE_ID", mylistdata.get(it))
                         context.startActivity(intent)
                     }
                 ) {
+                    ImageLoader("https://www.everblazing.org/wp-content/uploads/2017/06/avatar-372-456324.png")
+                    Spacer(modifier = Modifier.width(50.dp))
                     Text(
                         text = mylistdata.get(it).level + " | " + mylistdata.get(it).time,
                         modifier = Modifier.padding(8.dp)
 
                     )
+
 
                 }
             }
@@ -124,6 +128,39 @@ if(mylistdata!=null){
     }
 }
 }
+
+@ExperimentalCoilApi
+@Composable
+fun ImageLoader(imageUrl: String){
+
+    Box(modifier = Modifier
+        .height(150.dp)
+        .width(150.dp),
+        contentAlignment = Alignment.Center
+    ){
+
+        val painter1= rememberImagePainter(
+            data=imageUrl,
+            builder = {
+                error(R.drawable.error2)
+            }
+        )
+        val printState = painter1.state
+        Image(
+            painter = painter1,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.size(120.dp)
+
+        )
+
+        if (printState is ImagePainter.State.Loading){
+            CircularProgressIndicator()
+        }
+    }
+
+}
+
 
 
 
