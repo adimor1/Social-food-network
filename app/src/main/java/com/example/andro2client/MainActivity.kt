3645ab4b5a16a2c88d257ecb93c17385c2baefc8
adimor1 <adimor1>
2022-06-20 17:22:35 +0300

    logo

--- app/src/main/java/com/example/andro2client/MainActivity.kt
+++ app/src/main/java/com/example/andro2client/MainActivity.kt
@@ -7,6 +7,8 @@ import android.text.TextUtils
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -15,10 +17,6 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
@@ -29,6 +27,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.platform.testTag
@@ -37,6 +36,9 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.ImagePainter
+import coil.compose.rememberImagePainter
 import com.example.andro2client.Retrofit.MyService
 import com.example.andro2client.Retrofit.RetrofitClient
 import com.example.andro2client.model.LoginUser
@@ -98,6 +100,38 @@ fun LoginView(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.spacedBy(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+        Box(modifier = Modifier
+            .height(250.dp)
+            .width(300.dp),
+
+
+            contentAlignment = Alignment.Center
+        ){
+
+            val painter1= rememberImagePainter(
+                data="https://firebasestorage.googleapis.com/v0/b/andro2client.appspot.com/o/app%2Flogo.jpg?alt=media&token=8f9e729a-2d62-4578-9785-253a0238ed9a",
+                builder = {
+                    error(R.drawable.error2)
+                }
+            )
+            val printState = painter1.state
+            Image(
+
+                painter = painter1,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(300.dp)
+
+            )
+
+            if (printState is ImagePainter.State.Loading){
+                CircularProgressIndicator()
+            }
+        }
+
+        Text("InstaCook",  fontSize = 50.sp)
+
         OutlinedTextField(
             modifier = Modifier
                 .fillMaxWidth()
