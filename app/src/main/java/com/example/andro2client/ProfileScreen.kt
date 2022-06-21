f8a9adcf7c7766a98ac7741786d1316b72048c52
adimor1 <adimor1>
2022-06-21 19:55:21 +0300

    add bluev

--- app/src/main/java/com/example/andro2client/ProfileScreen.kt
+++ app/src/main/java/com/example/andro2client/ProfileScreen.kt
@@ -2,6 +2,7 @@ package com.example.andro2client
 
 import android.content.Context
 import android.content.Intent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
@@ -17,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
@@ -26,6 +28,8 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.toSize
+import coil.compose.ImagePainter
+import coil.compose.rememberImagePainter
 import com.example.andro2client.model.LoginUser
 import com.example.andro2client.model.Recipe
 import com.example.andro2client.model.User
@@ -91,12 +95,44 @@ fun ProfileView(user: User?){
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
+            Row() {
 
-            Text(
-                text = user.email,
-                fontSize = 25.sp
-            )
+                Box(
+                    modifier = Modifier
+                        .height(40.dp)
+                        .width(40.dp),
 
+
+                    contentAlignment = Alignment.Center
+                ) {
+
+                    val painter1 = rememberImagePainter(
+                        data = "https://firebasestorage.googleapis.com/v0/b/andro2client.appspot.com/o/app%2Ftwitter600.jpg?alt=media&token=95fd022b-62ee-4925-be05-2f4507450da0",
+                        builder = {
+                            error(R.drawable.error2)
+                        }
+                    )
+                    val printState = painter1.state
+                    Image(
+
+                        painter = painter1,
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.size(50.dp)
+
+                    )
+
+                    if (printState is ImagePainter.State.Loading) {
+                        CircularProgressIndicator()
+                    }
+                }
+                Spacer(modifier = Modifier.size(16.dp))
+
+                Text(
+                    text = user.email,
+                    fontSize = 25.sp
+                )
+            }
 
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth().testTag("name"),
